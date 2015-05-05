4b119f81ba923db60cf6705a25a9dcc96a21c3e5
TJ Tyrrell <=>
2015-05-04 22:43:47 -0700

    Expanded a lot of the page

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,18 +1,27 @@
+var home = 0,
+	installation = 0,
+	useage = 0,
+	contact = 0,
+	license = 0,
+	scrolling = false;
+
 $(document).on('click', '#cssmenu > ul > li > a', function( e ) {
 	
 	e.preventDefault();
-
+	scrolling = true;
 	var scrollToId = $(this).attr('href'),
 		scrollFrom = $(document).scrollTop(),
 		scrollTo = $( scrollToId ).offset().top,
-		scrollPixelsPerSecond = 500,
+		scrollPixelsPerSecond = 1500,
 		scrollRate = 0;
 
 	scrollRate = ( Math.abs( scrollTo - scrollFrom ) / scrollPixelsPerSecond ) * 1000;
 
 	$('html, body').animate({
         scrollTop: scrollTo
-    }, scrollRate);
+    }, scrollRate, function(){
+    	scrolling = false;
+    });
 
 	$('#cssmenu li').removeClass('active');
 	$(this).closest('li').addClass('active');	
@@ -38,5 +47,43 @@ $(document).on('click', '#cssmenu > ul > li > a', function( e ) {
 
 $(document).ready(function(){
 	
-	
+	installation = $('#installation').offset().top;
+	useage = $('#useage').offset().top;
+	contact = $('#contact').offset().top;
+	license = $('#license').offset().top;
+
+});
+
+$(document).on('scroll', function(e){
+	if( scrolling ) {
+		return false;
+	}
+	var curScroll = $(document).scrollTop();
+
+	if( curScroll >= 0 && curScroll < installation ) {
+		if( !$('a[href="#home"]').closest('li').hasClass('active') ) {
+			$('li.active').removeClass('active');
+			$('a[href="#home"]').closest('li').addClass('active');
+		}
+	} else if( curScroll >= installation && curScroll < useage ) {
+		if( !$('a[href="#installation"]').closest('li').hasClass('active') ) {
+			$('li.active').removeClass('active');
+			$('a[href="#installation"]').closest('li').addClass('active');
+		}
+	} else if( curScroll >= useage && curScroll < contact ) {
+		if( !$('a[href="#useage"]').closest('li').hasClass('active') ) {
+			$('li.active').removeClass('active');
+			$('a[href="#useage"]').closest('li').addClass('active');
+		}
+	} else if( curScroll >= contact && curScroll < license ) {
+		if( !$('a[href="#contact"]').closest('li').hasClass('active') ) {
+			$('li.active').removeClass('active');
+			$('a[href="#contact"]').closest('li').addClass('active');
+		}
+	} else if( curScroll >= license ) {
+		if( !$('a[href="#license"]').closest('li').hasClass('active') ) {
+			$('li.active').removeClass('active');
+			$('a[href="#license"]').closest('li').addClass('active');
+		}
+	}
 });
